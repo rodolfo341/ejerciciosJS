8d446e7e29061bbab18181c84df9e0b2aececf57
“Rodolfo <rodolfo.aravena.espinosa@gmail.com>
2023-04-30 23:09:33 -0400

    operador ternario anidado

--- app.js
+++ app.js
@@ -4,33 +4,10 @@ let x = parseInt(prompt("Ingrese X"));
 let y = parseInt(prompt("Ingrese Y"));
 let z = parseInt(prompt("Ingrese Z"));
 
-( x > y && x > z ) ? (document.getElementById("parrafo1").innerHTML = "X es mayor" ,
-                      document.getElementById("parrafo4").innerHTML = "X es mayorrrr",
-                      console.log("hola")): 
-( y > x && y > z ) ? (document.getElementById("parrafo1").innerHTML = "Y es mayor",
-                      document.getElementById("parrafo4").innerHTML = "Y es mayor",
-                      console.log("hola")) : 
-( z > x && z > y ) ? (document.getElementById("parrafo1").innerHTML = "Z es mayor",
-                      document.getElementById("parrafo4").innerHTML = "Y es mayorrrr",
-                      console.log("hola")) :
-                     (document.getElementById("parrafo1").innerHTML = "X = Y = Z",
-                      document.getElementById("parrafo4").innerHTML = "X === Y === Z",
-                      console.log("hola"));
-                     
 
-if ( x > y && x > z ) document.getElementById("parrafo2").innerHTML = "X es mayor" 
-else if ( y > x && y > z ) document.getElementById("parrafo2").innerHTML = "Y es mayor"
-else if ( z > x && z > y ) document.getElementById("parrafo2").innerHTML = "Z es mayor";
-else document.getElementById("parrafo2").innerHTML = "X = Y = Z";
+let mayor = ( x > y && x > z ) ? "El mayor es X" : 
+            ( y > x && y > z ) ? "El mayor es Y" :
+            ( z > x && z > y ) ? "El mayor es Z" :
+                                 "Son todos iguales" ;
 
-
-if ( x > y && x > z ){
-    document.getElementById("parrafo3").innerHTML = "X es mayor" 
-} else if ( y > x && y > z ) {
-    document.getElementById("parrafo3").innerHTML = "Y es mayor"
-}else if ( z > x && z > y ){
-    document.getElementById("parrafo3").innerHTML = "Z es mayor";
-}else {
-    document.getElementById("parrafo3").innerHTML = "X = Y = Z";
-}
-
+document.getElementById("parrafo2").innerHTML = mayor; 
